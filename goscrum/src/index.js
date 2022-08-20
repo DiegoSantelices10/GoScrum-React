53bc7821b8ccec3698e81f8a263b2df4ee13303c
DiegoSantelices10 <diego.santelices@davinci.edu.ar>
2022-08-20 14:05:45 -0300

    update

--- goscrum/src/index.js
+++ goscrum/src/index.js
@@ -11,7 +11,7 @@ import { store } from './store/store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="goscrum-react/">
+    <HashRouter basename="GoScrum-React/">
       <Provider store={store}>
         <App />
       </Provider>    
